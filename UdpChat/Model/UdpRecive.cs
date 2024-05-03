af91caa1e24ba6185c129ab575b9a12c870c8eca
Вячеслав Сквороцов <swank123@yandex.ru>
2024-05-03 18:47:10 +0700

    Добавьте файлы проекта.

---
+++ UdpChat/Model/UdpRecive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpChat.Model
+{
+    public class UdpRecive
+    {
+        private UdpClient _udpClient;
+
+        public UdpRecive(UdpClient udpClient)
+        {
+            ArgumentNullException.ThrowIfNull(udpClient);
+            _udpClient = udpClient;
+        }
+
+        public void StartRecive(string youName)
+        {
+
+        }
+    }
+}
---
+++ UdpChat/Model/UdpSender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpChat.Model
+{
+    public class UdpSender
+    {
+        private UdpClient _udpClient;
+
+        public UdpSender(UdpClient udpClient)
+        {
+            ArgumentNullException.ThrowIfNull(udpClient);
+            _udpClient = udpClient;
+        }
+
+        public void Send(string message) 
+        {
+            byte[] array = Encoding.UTF8.GetBytes(message);
+            _udpClient.Send(array, array.Length);
+            
+
+        }
+    }
+}
